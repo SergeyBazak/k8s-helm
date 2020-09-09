cbb1f0f9ff46dbdd7541bce729f380ec4b8ba9ee
Matt Butcher <matt.butcher@microsoft.com>
2020-09-09 16:47:59 -0600

    fixed bug that caused helm create to not overwrite modified files
    Signed-off-by: Matt Butcher <matt.butcher@microsoft.com>

--- pkg/chartutil/create.go
+++ pkg/chartutil/create.go
@@ -17,7 +17,8 @@ limitations under the License.
 package chartutil
 
 import (
-	"fmt"
+  "fmt"
+  "io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -468,6 +469,12 @@ spec:
   restartPolicy: Never
 `
 
+// StdErr is an io.Writer to which error messages can be written
+//
+// In Helm 4, this will be replaced. It is needed in Helm 3 to preserve API backward
+// compatibility.
+var Stderr io.Writer = os.Stderr
+
 // CreateFrom creates a new chart, but scaffolds it from the src chart.
 func CreateFrom(chartfile *chart.Metadata, dest, src string) error {
 	schart, err := loader.Load(src)
@@ -600,10 +607,10 @@ func Create(name, dir string) (string, error) {
 	}
 
 	for _, file := range files {
-		if _, err := os.Stat(file.path); err == nil {
-			// File exists and is okay. Skip it.
-			continue
-		}
+    if _, err := os.Stat(file.path); err == nil {
+      // There is no handle to a prefered output stream here.
+      fmt.Fprintf(Stderr, "WARNING: File %q already exists. Overwriting.\n", file.path)
+    }
 		if err := writeFile(file.path, file.content); err != nil {
 			return cdir, err
 		}
